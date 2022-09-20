07b502fbc90dc7d828d0e59c5575ea0b15d8f50f
Flávia <flaviaperelberg@hotmail.com>
2022-09-20 20:20:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
-  int cont, numero, maior=0;
+int main(void){
+  
+  int cont, numero, maior=0, menor=1;
+  
   for(cont=1; cont<=5; cont++){
-    printf("Digite um número: \n");
+    printf("Digite um número:\n");
     scanf("%d", &numero);
-
+ 
     if(numero>maior){
       maior=numero;
     }
-    
+    if(numero<menor){
+      menor=numero;
+    }
   }
-printf("O maior número é o: %d", maior);
+  
+printf("O maior número é: %d \n", maior);
+printf("O menor número é: %d", menor);
 }
