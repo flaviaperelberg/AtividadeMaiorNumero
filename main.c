f012d2952ba810c9fa1e6c214d9bb206cc7d1cc4
Flávia Perelberg <113394221+flaviaperelberg@users.noreply.github.com>
2022-10-13 19:11:25 +0000

    Declarei a variável fora do for.

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
-  int cont, numero, maior=0;
-  for(cont=1; cont<=5; cont++){
-    printf("Digite um número: \n");
-    scanf("%d", &numero);
+int main(void){
+  
+  int cont, numero, maior, menor;
 
+  printf("Digite um número:\n");
+  scanf("%d", &numero);
+  maior=numero;
+  menor=numero;
+  
+  for(cont=1; cont<=5; cont++){
+    printf("Digite um número:\n");
+    scanf("%d", &numero);
+    
     if(numero>maior){
       maior=numero;
     }
-    
+    if(numero<menor){
+      menor=numero;
+    }
   }
-printf("O maior número é o: %d", maior);
+  
+printf("O maior número é: %d \n", maior);
+printf("O menor número é: %d", menor);
+  
 }
